408ece8e9acad8aec193ffba0663e046df4bd957
Alju Sabu <aljusabu555@gmail.com>
2026-03-27 16:13:11 +0530

    scroll to top added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
-        <Routes>
+      <ScrollToTop />
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="signup" element={<Signup />} />
